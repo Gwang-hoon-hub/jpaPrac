1760a1e02ef90baeeaa8d694ed41c0f01567add0
panghoon <leegwanghoon95@gmail.com>
2022-07-17 23:10:28 +0900

    [FEAT] : Entity, Repository

---
+++ src/main/java/com/pang/jpaprac/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.pang.jpaprac.repository;
+
+import com.pang.jpaprac.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+
+}
