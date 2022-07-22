f45f5496b81d9ed2ba92f5f29ecae4e671c056be
panghoon <leegwanghoon95@gmail.com>
2022-07-23 01:45:51 +0900

    [FEAT] : I will go vacation!

---
+++ src/main/java/com/pang/jpaprac/domain/Category.java
@@ -0,0 +1,25 @@
+package com.pang.jpaprac.domain;
+
+import com.pang.jpaprac.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_id")
+    private List<Item> items = new ArrayList<Item>();
+}
